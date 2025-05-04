ea654305b6a8dd43ed04548e7dd70e3a7f95ea36
Johnny Faris <jkfaris@gmail.com>
2025-05-03 17:29:36 -0700

    finished connection walkthrough lesson

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,9 +1,10 @@
+const User = require("./users.model");
 
-function list(req, res) {
-
-  res.send("users");
+async function list(req, res) {
+  const users = await User.find();
+  res.send(users);
 }
 
 module.exports = {
-  list,
+  list
 };
