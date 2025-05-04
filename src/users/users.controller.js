ed968ce9fec3f179b1a86b8519877b6e462abfff
Johnny Faris <jkfaris@gmail.com>
2025-05-03 17:51:40 -0700

    created add user ability

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -5,6 +5,30 @@ async function list(req, res) {
   res.send(users);
 }
 
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`
+    });
+  };
+}
+
+async function create(req, res) {
+  const { data: { username, email } = {} } = req.body;
+  const newUser = new User({
+    username: username,
+    email: email
+  });
+  await newUser.save();
+  res.status(201).json({ data: newUser });
+}
+
 module.exports = {
-  list
+  list,
+  create: [bodyDataHas("username"), bodyDataHas("email"), create]
 };
